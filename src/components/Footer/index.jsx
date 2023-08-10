2c206829ffc6d18057d5addf25ee970c331bc32d
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-08-10 08:52:27 -0300

    cria diretorio de components

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+	return (
+		<footer className={styles.footer}>
+			<h4>Green Blog</h4>
+			<small>Green Blog &copy; 2023</small>
+		</footer>
+	)
+}
+
+export default Footer
