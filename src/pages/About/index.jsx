290c9e795961c754568d0e03667f4292e926835a
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-08-11 09:41:24 -0300

    pagina estatica, pagina sobre a aplicacao

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,32 @@
+import styles from './About.module.css'
+import { Link } from 'react-router-dom'
+
 const About = () => {
 	return (
-		<div>
-			<h1>About</h1>
+		<div className={styles.about}>
+			<h2>
+				Sobre o <span>green</span>Blog.
+			</h2>
+			<p>
+				O <span>green</span>Blog é um cativante projeto de mini blog
+				desenvolvido em React, utilizando os poderosos hooks para uma
+				experiência de desenvolvimento moderna e eficiente. Este blog dinâmico e
+				responsivo oferece aos usuários a oportunidade de compartilhar e
+				descobrir uma variedade de conteúdos relacionados ao mundo verde e
+				ambiental.
+			</p>
+			<p>
+				Com recursos intuitivos e interativos, os leitores podem explorar uma
+				coleção diversificada de postagens, abrangendo tópicos sustentáveis,
+				dicas ecológicas e novidades ambientais. A integração perfeita com o
+				Firebase permite um gerenciamento simplificado de autenticação de
+				usuários e armazenamento de dados em tempo real, garantindo uma
+				experiência fluida. Junte-se a nós no <span>green</span>Blog para se
+				inspirar, aprender e contribuir para um futuro mais sustentável.
+			</p>
+			<Link to="/posts/create" className="btn">
+				Create a new post
+			</Link>
 		</div>
 	)
 }
