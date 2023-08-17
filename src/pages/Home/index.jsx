189338e1366a8103825ce843b3d1e52f186049c5
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-08-17 10:16:41 -0300

    pagina home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,51 @@
+import styles from './Home.module.css'
+
+// hooks
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
+// componets
+import PostDetails from '../../components/PostDetails'
+
 const Home = () => {
+	const [query, setQuery] = useState('')
+	const { documents: posts, loading } = useFetchDocuments('posts')
+
+	const navigate = useNavigate()
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+
+		if (query) {
+			return navigate(`/search?q=${query}`)
+		}
+	}
+
 	return (
-		<div>
-			<h1>Home</h1>
+		<div className={styles.home}>
+			<h2>Latest articles</h2>
+			<form onSubmit={handleSubmit} className={styles.search_form}>
+				<input
+					type="text"
+					placeholder="Type your search"
+					onChange={(e) => setQuery(e.target.value)}
+				/>
+				<button className="btn btn-dark">Search</button>
+			</form>
+			<div className={styles.posts_area}>
+				{loading && <p>Loading</p>}
+				{posts &&
+					posts.map((post) => <PostDetails key={post.id} post={post} />)}
+				{posts && posts.length === 0 && (
+					<div className={styles.nopost}>
+						<p>Posts not found </p>
+						<Link to="/posts/create" className="btn">
+							Create you first post
+						</Link>
+					</div>
+				)}
+			</div>
 		</div>
 	)
 }
